f1a010f76975c23e92a87f2f1596cd2c0471f256
yujin <jini1514@soongsil.ac.kr>
2024-02-19 10:10:27 +0900

    vault backup: 2024-02-19 10:10:27

--- code/6.정렬/P.py
+++ code/6.정렬/P.py
@@ -1,11 +1,18 @@
-n=input()
+n=int(input())
+n_array =[]
+for i in range(n):
+    n_array.append(list(map(int,input().split())))
 
-array=[]
-for i in n:
-    array.append(i)
 
-array.sort(reverse=True)
-# print(array)
+m=int(input())
+m_array =[]
+for i in range(m):
+    m_array.append(list(map(int,input().split())))
 
-for i in array:
-    print(i,end="")
+cnt=[0]*len(m_array)
+for i in n_array:
+    for j in m_array:
+        if n_array[i] == m_array[j]:
+            cnt[m_array[j]]+=1
+
+print(cnt)

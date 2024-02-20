7ba5dde0937756e991724943e299045943a8b41c
yujin <jini1514@soongsil.ac.kr>
2024-02-20 14:26:49 +0900

    vault backup: 2024-02-20 14:26:49

--- code/6.정렬/P.py
+++ code/6.정렬/P.py
@@ -1,3 +1,4 @@
+from collections import Counter
 n=int(input())
 n_array =[]
 for i in range(n):
@@ -9,10 +10,10 @@ m_array =[]
 for i in range(m):
     m_array.append(list(map(int,input().split())))
 
-cnt=[0]*len(m_array)
-for i in n_array:
-    for j in m_array:
-        if n_array[i] == m_array[j]:
-            cnt[m_array[j]]+=1
+count = Counter(n_array)
 
-print(cnt)
+for i in m_array:
+    if i in count:
+        print(count[i], end = " ")
+    else: 
+        print(0, end=" ")
